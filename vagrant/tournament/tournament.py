1efa339786063db6ae1ab11869d89daea156ed94
thomasdunlap <tom.a.dunlap@gmail.com>
2017-12-06 20:26:50 -0500

    Finished connect() and deleteMatches() functions.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
+from random import shuffle
+#from contextlib import contextmanager
 
 import psycopg2
+import itertools
 
 
+#@contextmanager
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
@@ -13,7 +17,11 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    with connect() as database:
+        query = "TRUNCATE matches;"
+        database['cursor'].execute(query)
+        database['connection'].commit()
+        
 
 def deletePlayers():
     """Remove all the player records from the database."""
@@ -25,10 +33,10 @@ def countPlayers():
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -56,16 +64,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +81,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
