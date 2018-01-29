a220703575e9ddbed0ad8ec1661d3c524af88f9c
thomasdunlap <tom.a.dunlap@gmail.com>
2018-01-29 16:11:22 -0500

    feat: Added code to create db and players table.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,18 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Delete previously existing database
+DROP DATABASE IF EXISTS tournament;
 
+CREATE DATABASE tournament;
+
+-- Connect to database
+\c tournament;
+
+CREATE TABLE players (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+-- Make fake player if needed for bye games
+INSERT INTO players VALUES (0, 'bye game');
