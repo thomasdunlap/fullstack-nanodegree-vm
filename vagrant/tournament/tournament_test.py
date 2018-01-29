a63e6467ca7c4b4f3ec35ec8125bd654af1ffa8a
thomasdunlap <tom.a.dunlap@gmail.com>
2018-01-29 16:01:46 -0500

    chore: Minor formatting.

--- vagrant/tournament/tournament_test.py
+++ vagrant/tournament/tournament_test.py
@@ -152,4 +152,4 @@ if __name__ == '__main__':
     testStandingsBeforeMatches()
     testReportMatches()
     testPairings()
-    print "Success!  All tests pass!"
+    print "Success! All tests pass!"
